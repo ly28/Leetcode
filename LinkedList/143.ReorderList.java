97d51f85d937f02598e7bf6acc20d94f65bbfc74
LU YANG <luyang@LUs-MacBook-Pro.local>
2018-12-05 17:24:01 -0800

    aaaa

--- LinkedList/143.ReorderList.java
+++ LinkedList/143.ReorderList.java
@@ -1,7 +1,6 @@
 class ReorderList{    
     public void reorderList(ListNode head) {
         if(head==null||head.next==null) return;
-        
         //Find the middle of the list
         ListNode p1=head;
         ListNode p2=head;
@@ -9,7 +8,6 @@ class ReorderList{
             p1=p1.next;
             p2=p2.next.next;
         }
-        
         //Reverse the half after middle  1->2->3->4->5->6 to 1->2->3->6->5->4
         ListNode preMiddle=p1;
         ListNode preCurrent=p1.next;
