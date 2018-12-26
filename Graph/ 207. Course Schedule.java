c41be7d4b2cfb4924367b4bdc4211281c4728012
LU YANG <luyang@LUs-MacBook-Pro.local>
2018-12-26 11:29:19 -0800

    LC207

---
+++ Graph/ 207. Course Schedule.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList[] graph = new ArrayList[numCourses];
+        int[] visited = new int[numCourses];
+
+        for(int i = 0; i < numCourses; i++){
+            graph[i] = new ArrayList<Integer>();
+        }
+
+        for(int[] num : prerequisites){
+            int parent = num[1];
+            int child = num[0];
+            graph[parent].add(child);
+        }
+
+        for(int i = 0; i < numCourses; i++){
+            if(visited[i] == 0 && hasCycle(i, visited, graph)) return false;
+        }
+
+        return true;
+    }
+
+    private boolean hasCycle(int cur, int[] visited, ArrayList[] graph){
+        visited[cur] = 1;
+
+        for(int i = 0; i < graph[cur].size(); i++){
+            int next = (int) graph[cur].get(i);
+            if(visited[next] == 1) return true;
+            else if(visited[next] == 0 && hasCycle(next, visited, graph)) return true;
+        }
+
+        visited[cur] = 2;
+
+        return false;
+    }
+}
